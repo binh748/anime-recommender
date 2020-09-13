7dc5e362b718a4da61ea0e8fc867975835fe44b4
binh748 <binh.hoang7@gmail.com>
2020-09-13 16:34:58 -0400

    Made a lot of formatting improvements to Flask app--still need to add background image

--- flask/app.py
+++ flask/app.py
@@ -1,11 +1,14 @@
 import pickle
 from flask import Flask, redirect, url_for, request, render_template
-from recommendation import recommender as rec
+from recommendation.recommender import recommend
 app = Flask(__name__)
 
 with open('../pickles/rec_data.pkl', 'rb') as f:
     user_anime_cosine_distances_content, user_anime_cosine_distances_collab, \
         user_score_df, user_anime_history_df, anime_titles = pickle.load(f)
+
+with open('../pickles/cleaned_top_anime_data_1000_df.pkl', 'rb') as f:
+    top_anime_df = pickle.load(f)
 
 # To pass a variable into my request function, I need to put it into the URL
 @app.route('/recommendation/<user_id>/<adventurous_level>', methods=['POST', 'GET'])
@@ -16,13 +19,21 @@ def recommendation(user_id, adventurous_level):
                                 user_id=request.form.get('user_id'),
                                 adventurous_level=request.form.get('adventurous_level')))
 
-    recs, _ = rec.recommend(user_id, user_anime_cosine_distances_content,
-                            user_anime_cosine_distances_collab, user_score_df,
-                            user_anime_history_df, anime_titles,
-                            collab_weight=float(adventurous_level))
-    # Need to create a list of dicts storing the anime_title and image_url
-    # and then pass list of dicts into render_template to display anime title and image    
-    return render_template('recommendation.html', recs=recs, user_id=user_id,
+    recs, _ = recommend(user_id, user_anime_cosine_distances_content,
+                        user_anime_cosine_distances_collab, user_score_df,
+                        user_anime_history_df, anime_titles,
+                        collab_weight=float(adventurous_level))
+
+    recs_dicts = [
+        {'anime_title': anime_title,
+         'url': top_anime_df[top_anime_df['title_main'] == anime_title]['url'].values[0],
+         'image_url': top_anime_df[top_anime_df['title_main'] == anime_title]['image_url'].values[0]}
+        for anime_title in recs
+    ]
+
+    # Need to create a list of dicts to store anime_title and image_url
+    # and then pass list of dicts into render_template to display anime title and image
+    return render_template('recommendation.html', recs=recs_dicts, user_id=user_id,
                            adventurous_level=adventurous_level)
 
 @app.route('/', methods=['POST', 'GET'])
